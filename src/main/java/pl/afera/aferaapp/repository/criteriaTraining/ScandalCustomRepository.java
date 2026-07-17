d267300ccf374d417b97eeb4210ad418f3a91126
Blazej <blazejfran4@gmail.com>
2026-07-17 11:37:20 +0200

    fix(repository): training package deleted from 'main'

--- src/main/java/pl/afera/aferaapp/repository/criteriaTraining/ScandalCustomRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.afera.aferaapp.repository.criteriaTraining;
-
-import java.util.List;
-
-public interface ScandalCustomRepository {
-    List<PartyScandalSummary> getPartyScandalCount();
-}
